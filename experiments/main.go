a5c85c2d7f8c1567143179975733705cf6a6b463
Конев М.В. <konev.maks.v@gmail.com>
2024-09-18 01:08:45 +0400

    lab1 done

--- experiments/main.go
+++ experiments/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import "fmt" // format - вывод и ввод
 
 func main() {
-    fmt.Println("Hello, Go!")
+	fmt.Println("Hello, Go!")
+
+	println("Hello, Go!") // не рекомендуется ??
 }
---
+++ lab1/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Выберите задание:")
+	fmt.Println("1 - Вывести текущее время и дату")
+	fmt.Println("2 - Создать и вывести переменные различных типов")
+	fmt.Println("3 - Выполнить арифметические операции с целыми числами")
+	fmt.Println("4 - Вычислить сумму и разность чисел с плавающей запятой")
+	fmt.Println("5 - Вычислить среднее значение трех чисел")
+
+	var choice int
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case 1:
+		printCurrentTime()
+	case 2:
+		printVariables()
+	case 3:
+		performArithmetic()
+	case 4:
+		calculateSumAndDifference()
+	case 5:
+		calculateAverage()
+	default:
+		fmt.Println("Неверный выбор")
+	}
+}
+
+// Задание 1: вывод текущего времени и даты
+func printCurrentTime() {
+	currentTime := time.Now()
+	fmt.Println("Текущая дата и время:", currentTime.Format("02-01-2006 15:04:05"))
+}
+
+// Задание 2: создание и вывод переменных различных типов
+func printVariables() {
+	var integer int = 42
+	var floating float64 = 3.14
+	var str string = "Пример строки"
+	var boolean bool = true
+
+	// Использование краткой формы объявления переменных
+	x := 100
+	y := 12.34
+	z := "Краткая форма"
+	w := false
+
+	fmt.Println("Целое число:", integer)
+	fmt.Println("Число с плавающей запятой:", floating)
+	fmt.Println("Строка:", str)
+	fmt.Println("Булево значение:", boolean)
+
+	fmt.Println("Переменные, объявленные краткой формой:")
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+	fmt.Println("w:", w)
+}
+
+// Задание 3: арифметические операции с целыми числами
+func performArithmetic() {
+	var a, b int
+	fmt.Println("Введите два целых числа:")
+	fmt.Scanln(&a, &b)
+
+	fmt.Println("Сумма:", a+b)
+	fmt.Println("Разность:", a-b)
+	fmt.Println("Произведение:", a*b)
+	if b != 0 {
+		fmt.Println("Частное:", a/b)
+	} else {
+		fmt.Println("Деление на ноль!")
+	}
+}
+
+// Задание 4: сумма и разность чисел с плавающей запятой
+func calculateSumAndDifference() {
+	var x, y float64
+	fmt.Println("Введите два числа с плавающей запятой:")
+	fmt.Scanln(&x, &y)
+
+	fmt.Println("Сумма:", x+y)
+	fmt.Println("Разность:", x-y)
+}
+
+// Задание 5: вычисление среднего значения трех чисел
+func calculateAverage() {
+	var a, b, c float64
+	fmt.Println("Введите три числа для вычисления среднего значения:")
+	fmt.Scanln(&a, &b, &c)
+
+	average := (a + b + c) / 3
+	fmt.Println("Среднее значение:", average)
+}
